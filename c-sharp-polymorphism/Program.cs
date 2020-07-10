f40d67747931e3e2096cff86f9a3560089bf72de
kpnigalye <krishna.nigalye@gmail.com>
2020-07-10 18:26:58 +0530

    First Commit of Polymorphism code
    First Commit of Polymorphism code

---
+++ c-sharp-polymorphism/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace c_sharp_polymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Notification smsNotification = new SMSNotification();
+            smsNotification.NotifiyUser();
+
+            Notification emailNotification = new EmailNotification();
+            emailNotification.NotifiyUser();
+
+            Console.ReadLine();
+        }
+    }
+
+
+    public abstract class Notification
+    {
+        protected void GetUserContactInfo()
+        {
+            Console.WriteLine("Notification.GetUserContactInfo");
+        }
+
+        public abstract void NotifiyUser();
+    }
+
+    public class SMSNotification : Notification
+    {
+        public override void NotifiyUser()
+        {
+            GetUserContactInfo();
+            Console.WriteLine("SMS Notification\n");
+        }
+    }
+
+    public class EmailNotification : Notification
+    {
+        public override void NotifiyUser()
+        {
+            GetUserContactInfo();
+            Console.WriteLine("Email Notification\n");
+        }
+    }
+}
+
